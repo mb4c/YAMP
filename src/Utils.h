f462b4677078984cc82b172f8016267d8d028500
mb4c <marcin@marcinbac.pl>
2023-04-14 22:55:15 +0200

    Get metadata from title

--- src/Utils.h
+++ src/Utils.h
@@ -1,10 +1,11 @@
 #pragma once
-
+#include <regex>
 #include <string>
 #include <csignal>
 #include <pwd.h>
 #include <unistd.h>
 #include <filesystem>
+#include <iostream>
 
 inline std::string GetMusicFolderPath()
 {
@@ -91,4 +92,25 @@ inline void OpenInFileExplorer(std::filesystem::path path)
 	std::string command = "xdg-open \"" + path.parent_path().string() + "\"";
 	system(command.c_str());
 #endif
+}
+
+// Extracts artist, title, and track from a song title.
+// Expected format of the title string is "Number. Artist - Title".
+// Returns false if fails.
+inline bool GetMetadataFromTitle(std::string filename, unsigned int* track, std::string* artist, std::string* title)
+{
+	std::regex trackRegex("(\\d+)\\.? ?(.*) - (.*)");
+	std::smatch match;
+	if (std::regex_search(filename, match, trackRegex))
+		*track = std::stoi(match[1]);
+	else
+	{
+		std::cout << "Failed to get metadata from title:  " << filename << std::endl;
+		return false;
+
+	}
+
+	*artist = match[2];
+	*title = match[3];
+	return true;
 }
